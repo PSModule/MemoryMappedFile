680d18543ffdf003d33dc3ecbde48336635670c6
Marius Storhaug <marstor@hotmail.com>
2025-06-01 18:08:34 +0200

    Add test case for New-MemoryMappedFile function in MemoryMappedFile.Tests.ps1

--- tests/MemoryMappedFile.Tests.ps1
+++ tests/MemoryMappedFile.Tests.ps1
@@ -10,5 +10,13 @@
 param()
 
 Describe 'Module' {
-
+    Context 'MemoryMappedFile' {
+        It 'New-MemoryMappedFile' {
+            $fileName = 'test.mmf'
+            $size = 1024
+            $mmf = New-MemoryMappedFile -FileName $fileName -Size $size
+            $mmf | Should -Not -BeNullOrEmpty
+            Test-Path $fileName | Should -BeTrue
+        }
+    }
 }
