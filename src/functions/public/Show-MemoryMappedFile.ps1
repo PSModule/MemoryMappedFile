e7a69acfd505cbb855d92b4387cb3c65ea7d57eb
Marius Storhaug <marstor@hotmail.com>
2025-06-01 16:59:49 +0200

    Enhance documentation for Show-MemoryMappedFile function

--- src/functions/public/Show-MemoryMappedFile.ps1
+++ src/functions/public/Show-MemoryMappedFile.ps1
@@ -1,9 +1,34 @@
 ﻿function Show-MemoryMappedFile {
+    <#
+        .SYNOPSIS
+        Continuously displays the contents of a memory-mapped file in the console.
+
+        .DESCRIPTION
+        This function reads and displays the contents of a specified memory-mapped file in a loop,
+        refreshing the view at a user-defined interval. It uses `Read-MemoryMappedFileContent` to
+        retrieve data and clears the console after each refresh cycle.
+
+        .EXAMPLE
+        Show-MemoryMappedFile -Name 'SharedBuffer' -RefreshSeconds 2
+
+        Output:
+        ```powershell
+        Data: Hello from another process!
+        Timestamp: 2025-06-01T12:00:00
+        ```
+
+        Continuously displays the content of the 'SharedBuffer' memory-mapped file, refreshing every 2 seconds.
+
+        .LINK
+        https://psmodule.io/MemoryMapped/Functions/Show-MemoryMappedFile
+    #>
     [CmdletBinding()]
     param(
+        # The name of the memory-mapped file to read and display.
         [Parameter(Mandatory)]
         [string] $Name,
 
+        # The number of seconds to wait between refreshes of the displayed content.
         [Parameter()]
         [int] $RefreshSeconds = 1
     )
