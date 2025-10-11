66a828ce92d2d150c1b15a653cfd76fefb660c7d
Marius Storhaug <marstor@hotmail.com>
2025-10-11 09:06:52 +0200

    Remove placeholder test for New-MemoryMappedFile in MemoryMappedFile.Tests.ps1

--- tests/MemoryMappedFile.Tests.ps1
+++ tests/MemoryMappedFile.Tests.ps1
@@ -11,12 +11,8 @@ param()
 
 Describe 'Module' {
     Context 'MemoryMappedFile' {
-        It 'New-MemoryMappedFile' {
-            $fileName = '$HOME\test.json'
-            $size = 1024
-            $mmf = New-MemoryMappedFile -Name 'shared' -Path $fileName -Size $size
-            $mmf | Should -Not -BeNullOrEmpty
-            Test-Path $fileName | Should -BeTrue
+        It 'No tests yet...' {
+            Write-Host 'No tests yet...'
         }
     }
 }
